20d47a71e528339204d7f21e647045b95318fdd7
Harshwardhan kumar <smritigupta315@.com>
2024-10-27 22:13:52 +0530

    removed icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="one"><img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYwOqgKpxtLPgxcqgv7P96kf-j7OM9jycRHg&s"
+          class="image"
+        ></img>
+        </div>
+        <div class="two"><h1>FEEDBACK</h1></div>
+        <div class="head-btns">
+          <button class="head-btn">LOGOUT</button>
+          <button class="head-btn">BACK</button>
+        </div>
       </header>
+
+      <br></br>
+      <p class="styled-paragraph" style={{ textAlign: "center" }}>
+        How would you rate your website experience?
+      </p>
+      <div class="rating-container">
+        <div class="rating-option" data-rating="excellent">
+          <span class="emoji">😊</span>
+          <span class="label">Excellent</span>
+        </div>
+        <div class="rating-option" data-rating="good">
+          <span class="emoji">🙂</span>
+          <span class="label">Good</span>
+        </div>
+        <div class="rating-option" data-rating="ok">
+          <span class="emoji">😐</span>
+          <span class="label">OK</span>
+        </div>
+        <div class="rating-option" data-rating="bad">
+          <span class="emoji">🙁</span>
+          <span class="label">Bad</span>
+        </div>
+        <div class="rating-option" data-rating="worst">
+          <span class="emoji">😠</span>
+          <span class="label">Worst</span>
+        </div>
+      </div>
+      <br></br>
+      <p class="styled-paragraph">
+        Please share in detail what we can improve in our website experience
+      </p>
+
+      <div class="feedback-container">
+        <textarea
+          class="feedback-input"
+          placeholder="Write something..."
+        ></textarea>
+      </div>
+
+      <button class="submit-end">SUBMIT</button>
+      <br></br>
     </div>
   );
 }
